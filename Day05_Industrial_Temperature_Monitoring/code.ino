a7858894450227939c245694fc3a5f53e42559c2
KHUSHI B S <bskhushi2@gmail.com>
2026-03-12 21:15:56 +0530

    Create code.ino

---
+++ Day05_Industrial_Temperature_Monitoring/code.ino
@@ -0,0 +1,37 @@
+#include <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT22
+#define LED_PIN 8
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(LED_PIN, OUTPUT);
+
+  dht.begin();
+
+}
+
+void loop() {
+
+  float temperature = dht.readTemperature();
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" C");
+
+  if(temperature > 40)
+  {
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("Warning: Machine Overheating!");
+  }
+  else
+  {
+    digitalWrite(LED_PIN, LOW);
+  }
+
+  delay(2000);
+
+}
