bb31a7bb7ae77529e0d14cbc8696b3aa60076aad
KHUSHI B S <bskhushi2@gmail.com>
2026-04-26 23:49:51 +0530

    Update code.ino

--- Day45_Smart_Home_System/code.ino
+++ Day45_Smart_Home_System/code.ino
@@ -1,41 +1,87 @@
 #include <Servo.h>
-#define ledPin 7
-#define buzzer 6
-#define pirPin 2
-#define ldrPin A0
+
+#define LED_PIN 7
+#define SERVO_PIN 5
+#define PIR_PIN 2
+#define BUZZER_PIN 6
+#define LDR_PIN A0
+
 Servo fan;
+
+// thresholds
+int lightThreshold = 400;
+
+// states
+bool lightState = false;
+bool motionState = false;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
-  pinMode(buzzer, OUTPUT);
-  pinMode(pirPin, INPUT);
-  fan.attach(5);   
+
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(PIR_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  fan.attach(SERVO_PIN);
 }
 
 void loop() {
-  int light = analogRead(ldrPin);
-  if (light < 400) {
-    digitalWrite(ledPin, HIGH);
-    Serial.println("Light ON (Dark)");
-  } else {
-    digitalWrite(ledPin, LOW);
-    Serial.println("Light OFF");
-  }
-  if (light < 400) {
-    fan.write(180);  
-    Serial.println("Fan ON");
-  } else {
-    fan.write(0);   
-    Serial.println("Fan OFF");
-  }
-  int motion = digitalRead(pirPin);
-  if (motion == HIGH) {
-    digitalWrite(buzzer, HIGH);
-    Serial.println("Motion Detected!");
-  } else {
-    digitalWrite(buzzer, LOW);
-  }
 
-  delay(500);
+  int lightValue = analogRead(LDR_PIN);
+  int motion = digitalRead(PIR_PIN);
+
+  handleLighting(lightValue);
+  handleFan(lightValue);
+  handleSecurity(motion);
+
+  debugPrint(lightValue, motion);
+
+  delay(200);
+}
+
+// 💡 LIGHT SYSTEM
+void handleLighting(int lightValue) {
+
+  if (lightValue < lightThreshold) {
+    lightState = true;
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    lightState = false;
+    digitalWrite(LED_PIN, LOW);
+  }
+}
+
+// 🌀 FAN SYSTEM (SMOOTH CONTROL)
+void handleFan(int lightValue) {
+
+  int angle = map(lightValue, 0, 1023, 180, 0);
+  fan.write(angle);
+}
+
+// 🚨 SECURITY SYSTEM
+void handleSecurity(int motion) {
+
+  if (motion == HIGH) {
+    motionState = true;
+    digitalWrite(BUZZER_PIN, HIGH);
+  } else {
+    motionState = false;
+    digitalWrite(BUZZER_PIN, LOW);
+  }
+}
+
+// 📊 DEBUG (CLEAN OUTPUT)
+void debugPrint(int lightValue, int motion) {
+
+  Serial.print("Light: ");
+  Serial.print(lightValue);
+
+  Serial.print(" | LED: ");
+  Serial.print(lightState ? "ON" : "OFF");
+
+  Serial.print(" | Motion: ");
+  Serial.print(motion);
+
+  Serial.print(" | Alarm: ");
+  Serial.println(motionState ? "ON" : "OFF");
 }
