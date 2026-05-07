1a860569a84e60e0cc3c6627cf1ec40c805f55e4
KHUSHI B S <bskhushi2@gmail.com>
2026-05-07 23:38:02 +0530

    Create code.ino

---
+++ Day52_Interrupt_Based_Control/code.ino
@@ -0,0 +1,19 @@
+#define LED_PIN 7
+#define BUTTON_PIN 2
+volatile bool ledState = false;
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  attachInterrupt(
+    digitalPinToInterrupt(BUTTON_PIN),
+    toggleLED,
+    FALLING
+  );
+}
+
+void loop() {
+  digitalWrite(LED_PIN, ledState);
+}
+void toggleLED() {
+  ledState = !ledState;
+}
